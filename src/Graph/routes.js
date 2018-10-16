64331223da949f1e2c3464e81354fefce4f51987
Michael Nissen <m9ssen@gmail.com>
2018-10-16 15:55:43 +1100

    Added route for setGraphModel and changed naming collision

--- src/Graph/routes.js
+++ src/Graph/routes.js
@@ -1,5 +1,6 @@
 const Methods           = require('./methods');
 
 module.exports = (app) => {
+    app.route('graph/setGraphModel').post(Methods.setGraphModel);
     app.route('graph/getGraphModel').post(Methods.getGraphModel);
 };
