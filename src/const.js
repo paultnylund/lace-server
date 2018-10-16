980147f4f94f7a2d88aa423e4fb274175d2d8f1a
Michael Nissen <m9ssen@gmail.com>
2018-10-16 16:00:10 +1100

    Created a file with error constants

---
+++ src/const.js
@@ -0,0 +1,9 @@
+const DATA_UNDEFINED = 'No data was passed through';
+const DATA_NOT_EXISTS = 'The data you were tyring to find does not exist';
+const INSERT_ERROR = 'There was an error inserting to the database';
+
+module.exports = {
+    DATA_UNDEFINED,
+    DATA_NOT_EXISTS,
+    INSERT_ERROR,
+};
