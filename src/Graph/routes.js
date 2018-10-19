6466e11dacf8c54c0f7b221bbc9f29d7576bb0d1
Michael Nissen <m9ssen@gmail.com>
2018-10-19 11:48:30 +1100

    Changed routes

--- src/Graph/routes.js
+++ src/Graph/routes.js
@@ -1,6 +1,6 @@
 const Methods           = require('./methods');
 
 module.exports = (app) => {
-    app.route('/api/graph/setGraphModel').post(Methods.setGraphModel);
-    app.route('/api/graph/getGraphModel').post(Methods.getGraphModel);
+    app.route('graph/setGraphModel').post(Methods.setGraphModel);
+    app.route('graph/getGraphModel').post(Methods.getGraphModel);
 };
