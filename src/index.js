32af9dbb96b5a2c95a7192e9eb7278e965c34dad
Michael Nissen <m9ssen@gmail.com>
2018-10-19 11:20:37 +1100

    Testing API server

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ api.use((req, res, next) => {
 Graph.route(api);
 
 const apiServer = https.createServer(credentials, api);
-const webServer = https.createServer(credentials, web);
+const webServer = http.createServer(web);
 
 // apiServer.listen(API_PORT, 'localhost', () => {
 //     console.log(`API Server is running on port ${API_PORT}`);
