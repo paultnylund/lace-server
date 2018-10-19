5ee7fc5439ecbd22227c3f409bf9cf41a7536b12
Michael Nissen <m9ssen@gmail.com>
2018-10-19 15:32:07 +1100

    Just manually added the build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const path              = require('path');
 const compression       = require('compression');
 const bodyParser        = require('body-parser');
 const mongoose          = require('mongoose');
-const http             = require('http');
+const http              = require('http');
 const Graph             = require('./Graph');
 
 const AUTHCODE = "afjCEsnkK3bJ@#$dz%3JRTMtWJIAZs@Cc$Me*%!KkXpNR9G1MS$2xtfn5!FfGsy!caK5#kVd4l%ghDyFWp2jAVGaPYdAaerCDW9Snu0G#IOXVBIb*uCx5gt7O0&c1&tUg#G7Nd5nUHTQM7d32nzRlRa3D&WqWN9y&Bqe3SCv7C*mS4LFV5kM37wFbgDgvjELZI%mvx*v&a!w0Ie3XWy$Gdu6NJJUJ#eN^&Q!pCUVyWkZ9B7py8p^a*92r80iOrX3v@BSREqS^MEkx3$#2kUtP%#X5Oq!L*Ovg9Fg5$6xR0oX";
@@ -81,15 +81,11 @@ api.use((req, res, next) => {
 Graph.route(api);
 
 const apiServer = http.createServer(api);
-
-// web.use((req, res, next) => {
-//     return (res.json('HELLOOOO'));
-// });
 const webServer = http.createServer(web);
 
-apiServer.listen(API_PORT, '127.0.0.1', () => {
+apiServer.listen(WEB_PORT, '127.0.0.1', () => {
     console.log(`API Server is running on port ${API_PORT}`);
 });
-webServer.listen(WEB_PORT, '127.0.0.1', (res) => {
+webServer.listen(API_PORT, '127.0.0.1', (res) => {
     console.log(`WEB Server is running on port ${WEB_PORT}`);
 });
