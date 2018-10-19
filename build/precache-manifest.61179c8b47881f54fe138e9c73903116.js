6380ee10ad954316b94189f89744b5d638e9b9ec
Michael Nissen <m9ssen@gmail.com>
2018-10-19 13:36:36 +1100

    Just manually added the build

---
+++ build/precache-manifest.61179c8b47881f54fe138e9c73903116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f35f1f651d45bde5098b",
+    "url": "/static/js/main.f35f1f65.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "f35f1f651d45bde5098b",
+    "url": "/static/css/main.f0dcfe4b.chunk.css"
+  },
+  {
+    "revision": "14f1859b6cf73837e81086dc78610e6f",
+    "url": "/index.html"
+  }
+];
