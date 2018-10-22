1c1f7f4a65eaac3a19e3f4e86bf6df8e1b9b5061
Michael Nissen <m9ssen@gmail.com>
2018-10-22 13:15:29 +1100

    Fixed naming error in stream routes

--- src/Stream/routes.js
+++ src/Stream/routes.js
@@ -1,6 +1,6 @@
 const Methods            = require('./methods');
 
 module.exports = (app) => {
-    app.route('/stream/streamAndDetect').post(Methods.streamImage);
+    app.route('/stream/streamAndDetect').post(Methods.streamAndDetect);
     // app.route('/stream/stream').post(Methods.stream);
 };
