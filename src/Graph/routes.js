ea47dabb364d2cda21a4a2586128a0fdc0a6667a
Michael Nissen <m9ssen@gmail.com>
2018-10-23 10:37:36 +1100

    Testing stream api

--- src/Graph/routes.js
+++ src/Graph/routes.js
@@ -2,6 +2,5 @@ const Methods           = require('./methods');
 
 module.exports = (app) => {
     app.route('/graph/setGraphModel').post(Methods.setGraphModel);
-    app.route('/graph/streamAndDetect').post(Methods.streamAndDetect);
     app.route('/graph/getGraphModel').post(Methods.getGraphModel);
 };
