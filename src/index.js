c098de0130be5ffbde008f07c137ddfcf0d96041
Michael Nissen <m9ssen@gmail.com>
2018-10-22 15:31:13 +1100

    Enabled cors on the web server

--- src/index.js
+++ src/index.js
@@ -82,6 +82,13 @@ api.use((req, res, next) => {
 	next();
 });
 
+web.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	next();
+})
+
 GRAPH.route(api);
 STREAM.route(api);
 
