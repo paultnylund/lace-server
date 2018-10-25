e8df3d0aaf02d036860c8a3cbf4cb8b85d1be2ee
Michael Nissen <m9ssen@gmail.com>
2018-10-25 16:49:29 +1100

    Testing auth methods

--- src/Users/model.js
+++ src/Users/model.js
@@ -3,6 +3,7 @@ const mongoose          = require('mongoose');
 const Users = new mongoose.Schema({
     username:       { type: String, required: true },
     password:       { type: String, required: true },
+    salt:           { type: String, required: true },
 });
 
 module.exports = mongoose.model('Users', Users);
