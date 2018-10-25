65f3776641e3b98464277a75d46a709928a9524c
Michael Nissen <m9ssen@gmail.com>
2018-10-25 16:29:07 +1100

    Testing auth methods

--- src/Users/methods.js
+++ src/Users/methods.js
@@ -5,37 +5,21 @@ const CONST				= require('../const.js');
 
 exports.connectUser = (req, res) => {
     const data = req.body;
-    // console.log(data);
 
     if (!data) {
         return (res.send({ error: CONST.DATA_UNDEFINED }));
     }
-
-    // USERS.aggregate([
-    //     {
-    //         $project: {
-    //             _id: '$_id',
-    //             username: '$username',
-    //             password: '$password',
-    //             salt: '$salt',
-    //         },
-    //     },
-    // ], (error, result) => {
-    //     console.log(error);
-    //     console.log(result);
-    // })
 
     USERS.findOne({ username: data.username.toLowerCase() }, (error, result) => {
         if (error) {
             return (res.send(error));
         }
 
-        console.log(error);
-        console.log(result);
-
         if (!result) {
             return (res.send({ error: CONST.USER_NOT_EXISTS }));
         }
+
+        console.log(result.salt)
 
         const key512Bits = CryptoJS.PBKDF2(`${data.password}`, `${result.salt}`, {
             keySize: 512 / 32,
