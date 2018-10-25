f4de9652e13caedc0dd571022f7ebbcd35acc8e5
Michael Nissen <m9ssen@gmail.com>
2018-10-25 16:26:53 +1100

    Testing auth methods

--- src/Users/methods.js
+++ src/Users/methods.js
@@ -11,45 +11,43 @@ exports.connectUser = (req, res) => {
         return (res.send({ error: CONST.DATA_UNDEFINED }));
     }
 
-    USERS.aggregate([
-        {
-            $project: {
-                _id: '$_id',
-                username: '$username',
-                password: '$password',
-                salt: '$salt',
-            },
-        },
-    ], (error, result) => {
-        console.log(error);
-        console.log(result);
-    })
-
-    // USERS.findOne({
-    //     username: data.username.toLowerCase()
-    // }, (error, result) => {
-    //     if (error) {
-    //         return (res.send(error));
-    //     }
-
+    // USERS.aggregate([
+    //     {
+    //         $project: {
+    //             _id: '$_id',
+    //             username: '$username',
+    //             password: '$password',
+    //             salt: '$salt',
+    //         },
+    //     },
+    // ], (error, result) => {
     //     console.log(error);
     //     console.log(result);
+    // })
 
-    //     if (!result) {
-    //         return (res.send({ error: CONST.USER_NOT_EXISTS }));
-    //     }
+    USERS.findOne({ username: data.username.toLowerCase() }, (error, result) => {
+        if (error) {
+            return (res.send(error));
+        }
 
-    //     const key512Bits = CryptoJS.PBKDF2(`${data.password}`, `${result.salt}`, {
-    //         keySize: 512 / 32,
-    //         iterations: 1000,
-    //     });
+        console.log(error);
+        console.log(result);
 
-    //     if (key512Bits === result.password) {
-    //         console.log('MATCHING');
-    //         return (res.send(result));
-    //     }
+        if (!result) {
+            return (res.send({ error: CONST.USER_NOT_EXISTS }));
+        }
 
-    //     console.log('NOT MATCHING');
-    //     return (res.send({ error: CONST.USER_NOT_EXISTS }));
-    // });
+        const key512Bits = CryptoJS.PBKDF2(`${data.password}`, `${result.salt}`, {
+            keySize: 512 / 32,
+            iterations: 1000,
+        });
+
+        if (key512Bits === result.password) {
+            console.log('MATCHING');
+            return (res.send(result));
+        }
+
+        console.log('NOT MATCHING');
+        return (res.send({ error: CONST.USER_NOT_EXISTS }));
+    });
 };
