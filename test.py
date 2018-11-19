37b525157226afe8355d8b8f55bc3c7ab2daca94
Michael Nissen <m9ssen@gmail.com>
2018-11-20 09:53:41 +1100

    Testing ARG_MAX size of the system

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def load_image_into_numpy_array(image):
 # generated_grid = grid.create_grid_boxes_array(30)
 
 # generated_grid = grid.create_grid_boxes_array(5)
-
+print(sys.getsizeof(sys.argv[1]))
 print(sys.argv[1])
 
 boundingboxes_test = [
