1a80263d96000eb4d17f2b1fd79bf60e63610395
Michael Nissen <m9ssen@gmail.com>
2018-11-20 10:49:17 +1100

    Testing the new image storage implementation

--- exec.py
+++ exec.py
@@ -10,23 +10,23 @@ import detect
 # import grid tools
 
 # Take the input image
-image_path = 'detection_images/detection.jpg'
+image_path = 'detection_images/detection1.jpg'
 # print('HIIIII')
 # image_data = re.sub('^data:image/.+;base64,', '', data).decode('base64')
 # image_path = 'object_detection/test_images/image1_old.jpg'
 # image = Image.open(BytesIO(base64.b64decode(data)))
 # image = Image.open(cStringIO.StringIO(image_data))
 image = Image.open(image_path)
-print(image)
+sys.stdout.write(image)
 
 # Run object detection on the image which returns the bounding boxes
-detection_results = detect.run_object_detection_on_image(image)
-bounding_boxes = detection_results['bounding_boxes']
-classes = detection_results['classes']
-distance = detection_results['node_distance']
+# detection_results = detect.run_object_detection_on_image(image)
+# bounding_boxes = detection_results['bounding_boxes']
+# classes = detection_results['classes']
+# distance = detection_results['node_distance']
 
-# Run grid tools on with the bounding boxes which returns density graph
-density_grid = grid.create_density_grid(bounding_boxes, classes, distance, 21)
-# print(bounding_boxes)
-# Flush the density graph to stdout
-sys.stdout.write(density_grid)
+# # Run grid tools on with the bounding boxes which returns density graph
+# density_grid = grid.create_density_grid(bounding_boxes, classes, distance, 21)
+# # print(bounding_boxes)
+# # Flush the density graph to stdout
+# sys.stdout.write(density_grid)
