4df067648a7af1827e02727f4393bd58de303bfc
Michael Nissen <m9ssen@gmail.com>
2019-01-01 09:58:06 +0100

    Testing the detection routine

--- exec.py
+++ exec.py
@@ -24,7 +24,7 @@ detection_results = detect.run_object_detection_on_image(image)
 bounding_boxes = detection_results['bounding_boxes']
 classes = detection_results['classes']
 # distance = detection_results['node_distance']
-distance = 7 / 21
+distance = 0.330
 
 # Run grid tools on with the bounding boxes which returns density graph
 density_grid = grid.create_density_grid(bounding_boxes, classes, distance, 21)
