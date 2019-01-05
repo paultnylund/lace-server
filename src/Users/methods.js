a51311478ce821174991a8084227cecebdae5e3a
Michael Nissen <m9ssen@gmail.com>
2019-01-05 16:38:18 +0100

    Testing connectUser method

--- src/Users/methods.js
+++ src/Users/methods.js
@@ -6,7 +6,7 @@ const CONST				= require('../const.js');
 exports.connectUser = (req, res) => {
     const data = req.body;
     
-    console.log(data.entries().length);
+    console.log(Object.entries(data).length);
     if (data.entries().length === 0) {
         return (res.send({ error: CONST.DATA_UNDEFINED }));
     }
