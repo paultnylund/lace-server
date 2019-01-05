5886e7ba8e486b287651288d31500ee9abf9cef6
Michael Nissen <m9ssen@gmail.com>
2019-01-05 16:58:24 +0100

    connectUser method is now working again

--- src/Users/methods.js
+++ src/Users/methods.js
@@ -9,14 +9,11 @@ exports.connectUser = (req, res) => {
     if (Object.entries(data).length === 0) {
         return (res.send({ error: CONST.DATA_UNDEFINED }));
     }
-
-    console.log(data);
     
     USERS.findOne({ username: data.username.toLowerCase() }, (error, result) => {
         if (error) {
             return (res.send(error));
         }
-        console.log(result);
         
         if (!result) {
             return (res.send({ error: CONST.USER_NOT_EXISTS }));
@@ -26,7 +23,6 @@ exports.connectUser = (req, res) => {
             keySize: 512 / 32,
             iterations: 1000,
         });
-        console.log(key512Bits.toString());
         
         if (key512Bits.toString() === result.password) {
             return (res.send(result));
