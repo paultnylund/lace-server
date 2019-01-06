91745d4a9bec96b5f431a92204cc43d417ef4e49
Michael Nissen <m9ssen@gmail.com>
2019-01-06 14:20:14 +0100

    Fixed missing params definition in stream storage method

--- src/Stream/methods.js
+++ src/Stream/methods.js
@@ -120,8 +120,8 @@ function handleStreamStorage(image, id, boundingBoxes, gridBoxes) {
 	// 	}
 	// });
 
+	let params = {};
 	s3.listObjectsV2(params, function(listError, listResult) {
-		let params = { };
 
 		if (listError) {
 			console.log(listError);
